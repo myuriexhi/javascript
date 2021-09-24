f2437e9aacd0dfffd258a295c5ca2424a3b9a6c2
mayuri <mayuri03.exhibyte@gmail.com>
2021-09-24 16:52:26 +0530

    add file

--- GUESS MY- NUMBER/script.js
+++ GUESS MY- NUMBER/script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretnumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +28,25 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    //when guess is wrong
+    /*
+    else if (guess !== secretnumber) {
+      if (score > 1) {
+        document.querySelector('.message').textContent =
+          guess > secretnumber ? 'too high!' : 'too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'you lost the game!';
+        document.querySelector('.score').textContent = 0;
+      }
+    }
+  */
 
     //when guess to high
   } else if (guess > secretnumber) {
@@ -62,18 +82,3 @@ document.querySelector('.again').addEventListener('click', function () {
 
   document.querySelector('.number').style.width = '15rem';
 });
-
-//codding challenges#1
-/////////////////////////////////////
-// Coding Challenge #1
-
-/* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
-
-1. Select the element with the 'again' class and attach a click event handler
-2. In the handler function, restore initial values of the score and secretNumber variables
-3. Restore the initial conditions of the message, number, score and guess input field
-4. Also restore the original background color (#222) and number width (15rem)
-
-GOOD LUCK 😀
-*/
